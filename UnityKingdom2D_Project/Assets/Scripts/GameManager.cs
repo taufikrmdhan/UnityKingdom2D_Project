2894fb0c5be2c897d30997157c205b3593c671a8
taufik_rmdhan <taufik.30@students.amikom.ac.id>
2022-04-07 21:05:32 +0700

    player gak mau mati

---
+++ UnityKingdom2D_Project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Origin_SkeltCreep => Resources.Load<GameObject>("Prefabs/Enemies/skeltCreep");
+    public GameObject Origin_CastLight => Resources.Load<GameObject>("Prefabs/Stuffs/CastLight");
+
+    public static GameManager Instance { get; private set; }
+    private void Start()
+    {
+        Instance = this;
+    }
+}
+
+public enum EnemySet
+{
+    Default, Damaged, Native, Warrior, Witch, Skeleton,
+}
+
+public enum UnitState
+{
+    Idle, Move, Attack, Cast, Dead,
+}
---
+++ UnityKingdom2D_Project/Assets/Scripts/HitHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitHandler : MonoBehaviour
+{
+    public float AttackPoint;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && collision.isTrigger)
+        {
+            var player = collision.GetComponent<PlayerActive>();
+            player.HealthPoint -= AttackPoint;
+        }
+    }
+}
